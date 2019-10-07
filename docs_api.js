b3f886efec7b9a40342dc61b32e59eae4f54b692
Yashar <yazemoo1@gmail.com>
2019-10-07 12:27:57 -0700

    Revert to a previous commit

--- docs_api.js
+++ docs_api.js
@@ -1,9 +1,24 @@
+<<<<<<< HEAD
 /* Array of API discovery doc URLs for APIs used by the quickstart */
 const DISCOVERY_DOCS = ['https://docs.googleapis.com/$discovery/rest?version=v1'];
 const authorizeButton = $('#authorize_button');
 const signoutButton = $('#signout_button');
 const textarea = $('#textarea');
 const submitButton = $('#submitCode');
+=======
+/* Client ID and API key from the Developer Console */
+var CLIENT_ID = '233676235337-e4k8et9oj9p8d4pq6i89kbiifn5hmqe3.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyAh7uT_LoX_U0LxWpEw0jGLCxTpUMQSIOs';
+
+/* Array of API discovery doc URLs for APIs used by the quickstart */
+var DISCOVERY_DOCS = ['https://docs.googleapis.com/$discovery/rest?version=v1'];
+
+/* Authorization scopes required by the API; multiple scopes can be included, separated by spaces. */
+var SCOPES = "https://www.googleapis.com/auth/drive.file";
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+>>>>>>> parent of fc42c5b... Update docs_api.js
 /**
 *  On load, called to load the auth2 library and API client library.
 */
@@ -34,8 +49,13 @@ function initClient() {
 		apiKey: "AIzaSyAh7uT_LoX_U0LxWpEw0jGLCxTpUMQSIOs",
 		clientId: "233676235337-e4k8et9oj9p8d4pq6i89kbiifn5hmqe3.apps.googleusercontent.com",
 		discoveryDocs: DISCOVERY_DOCS,
+<<<<<<< HEAD
 		scope: "https://www.googleapis.com/auth/drive.file"
 	}).then(() => {
+=======
+		scope: SCOPES
+	}).then(function() {
+>>>>>>> parent of fc42c5b... Update docs_api.js
 		/* Listen for sign-in state changes. */
 		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 		/* Handle the initial sign-in state. */
@@ -75,11 +95,11 @@ function submitCode() {
 function printDocTitle() {
 	gapi.client.docs.documents.get({
 		documentId: '195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE'
-	}).then(response => {
+	}).then(function(response) {
 		var doc = response.result;
 		var title = doc.title;
 		appendPre('Document "' + title + '" successfully found.\n');
-	}, response => {
+	}, function(response) {
 		appendPre('Error: ' + response.result.error.message);
 	});
 }*/
