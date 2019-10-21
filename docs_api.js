e814bc2371b38a9779db71a1f0e00a386db34d48
platipus25 <40722163+platipus25@users.noreply.github.com>
2019-10-20 22:04:36 -0700

    Use async/await

--- docs_api.js
+++ docs_api.js
@@ -14,21 +14,20 @@ function handleClientLoad() {
 /**
 *  Initializes the API client library and sets up sign-in state listeners.
 */
-function initClient() {
-	gapi.client.init({
+async function initClient() {
+	await gapi.client.init({
 		apiKey: "AIzaSyBbOErwKsigaldMEcmsoNURF9DHED17p-0",
 		clientId: "233676235337-e4k8et9oj9p8d4pq6i89kbiifn5hmqe3.apps.googleusercontent.com",
 		discoveryDocs: ["https://docs.googleapis.com/$discovery/rest?version=v1", "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
 		scope: "https://www.googleapis.com/auth/drive.file"
-	}).then(() => {
-		// Listen for sign-in state changes.
-		gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+	})
+	// Listen for sign-in state changes.
+	gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-		// Handle the initial sign-in state.
-		updateSigninStatus();
-		authorizeButton.onclick = handleAuthClick;
-		signoutButton.onclick = handleSignoutClick;
-	});
+	// Handle the initial sign-in state.
+	updateSigninStatus();
+	authorizeButton.onclick = handleAuthClick;
+	signoutButton.onclick = handleSignoutClick;
 }
 
 /**
@@ -65,7 +64,7 @@ function handleSignoutClick(event) {
 /**
 * Creates a document with the log
 */
-function createDoc() {
+async function createDoc() {
 	if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
 		alert("Please sign in to use the feature");
 		window.location.reload(false);
@@ -73,27 +72,33 @@ function createDoc() {
 	
 	//Searches for a folder to put the logs in. If none exist, it will create one
 	let folderId;
-	gapi.client.drive.files.list({
-		"q": "name = 'Daily Logs' and mimeType = 'application/vnd.google-apps.folder'"
-	}).then(response => {
-		console.log("Response", response);
-		if (response.result.files.length != 0) {
-			folderId = response.result.files[0].id;
-			console.log("i found it");
-		} else {
-			console.log("i didnt find it");
-			gapi.client.drive.files.create({
+	let response = null;
+	try {
+		response = await gapi.client.drive.files.list({
+			"q": "name = 'Daily Logs' and mimeType = 'application/vnd.google-apps.folder'"
+		})
+	} catch(err){
+		console.error("Execute error", err);
+		return;
+	}
+	console.log("Response", response);
+	if (response.result.files.length != 0) {
+		folderId = response.result.files[0].id;
+		console.log("i found it");
+	} else {
+		console.log("i didnt find it");
+		let response1 = null;
+		try {
+			response1 = await gapi.client.drive.files.create({
 				"mimeType": "application/vnd.google-apps.folder",
 				"name": "Daily Logs"
-			}).then(response1 => {
-				folderId = response1.id;
-			}, err => {
-				console.error("Execute error", err);
-			});
+			})
+		} catch (err){
+			console.error("Execute error", err);
+			return;
 		}
-	}, err => { 
-		console.error("Execute error", err);
-	});
+		folderId = response1.id;
+	}
 	console.log(folderId);
 	
 	//Creates the google doc
@@ -103,27 +108,30 @@ function createDoc() {
 	let documentId;
 	
 	const searchQuery = "name = '" + title + "' and mimeType = 'application/vnd.google-apps.document' and '" + folderId + "' in parents";
-	gapi.client.drive.files.list({
-		"q": searchQuery
-	}).then(response => {
-		console.log("Response", response);
-		if (response.result.files.length != 0) {
-			documentId = response.result.files[0].id;
-			console.log("i found it");
-		} else {
-			console.log("i didnt find it");
-			gapi.client.drive.files.create({
-				"mimeType": "application/vnd.google-apps.document",
-				"name": title
-			}).then(response1 => {
-				documentId = response1.id;
-			}, err => {
-				console.error("Execute error", err);
-			});
-		}
-	}, err => { 
+	let response = null;
+	try {
+		response = gapi.client.drive.files.list({
+			"q": searchQuery
+		})
+	} catch (err){
 		console.error("Execute error", err);
-	});
+		return;
+	}
+	console.log("Response", response);
+	if (response.result.files.length != 0) {
+		documentId = response.result.files[0].id;
+		console.log("i found it");
+	} else {
+		console.log("i didnt find it");
+		gapi.client.drive.files.create({
+			"mimeType": "application/vnd.google-apps.document",
+			"name": title
+		}).then(response1 => {
+			documentId = response1.id;
+		}, err => {
+			console.error("Execute error", err);
+		});
+	}
 	const params = {"documentId": documentId};
 	
 	// Body of the request to batch update
@@ -145,9 +153,16 @@ function createDoc() {
 	textarea.value = "";
 	
 	// Batch updates
-	gapi.client.docs.documents.batchUpdate(params, updateRequest).then(response => {
+	let response = null
+	try{
+		gapi.client.docs.documents.batchUpdate(params, updateRequest)
+	} catch (err){
+		console.error("Execute error", err);
+		return;
+	}
+	if(response.result.error.message) {
+		console.error("Error: " + response.result.error.message);
+	}else {
 		console.log("Document sucessfully batch updated");
-	}, () => {
-		console.log("Error: " + response.result.error.message);
-	});
+	}
 }
